1c25ec09fc27402353272df9f2c5023ef9faafe9
juthawong <juthawong@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-03-04 03:43:39 +0000

    Update Defa Protector
    git-svn-id: https://plugins.svn.wordpress.org/defa-protector-protect-html5-video-from-download-for-free-opensource/trunk@1104661 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- defaprotector.php
+++ defaprotector.php
@@ -1,9 +1,9 @@
 <?php
 /*
-Plugin Name: Defa Protector V.4 Light
+Plugin Name: Defa Protector Platinum
 Plugin URI: http://ww2.juthawong.com/defaprotector
 Description: Protect your video from being downloaded or stolen
-Version: 3.0
+Version: 4.0
 Author: Juthawong Naisanguansee
 Author URI: http://www.juthawong.com/
 License: GPL
