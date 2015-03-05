9056e34dc90ed318ac7ac707891f9051c2cab1ad
juthawong <juthawong@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-03-05 07:21:25 +0000

    Update Plugin url
    git-svn-id: https://plugins.svn.wordpress.org/defa-protector-protect-html5-video-from-download-for-free-opensource/trunk@1105772 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includebottom.php
+++ includebottom.php
@@ -69,14 +69,16 @@ $_SESSION['defat'] = 1;
 }else{
 $_SESSION['defat'] = $_SESSION['defat'] + 1;
 }
-
+$file = dirname(__FILE__) . '/defaprotector.php';
+$plugin_url= plugin_dir_url($file);
+$plugin_url = wp_make_link_relative($plugin_url);
 $_SESSION['x'.$matches['2'].$_SESSION['defat']]=0;
 $_SESSION['defa'.$matches['2'].$_SESSION['defat']] = md5(time()."Defa Protector");
 $_SESSION['imdefa'.$_SESSION['defat']]=md5('Defa').base64_encode(base64_encode($matches['2']));
 $_SESSION['x'.$matches['2']]=0;
 $_SESSION['defa'.$matches['2']] = md5(time()."Defa Protector");
 $_SESSION['file'.$_SESSION['defat']] = md5('Defa').base64_encode(base64_encode($matches['2']));
-  return $matches[1] . $rootURL  . "/defavid.php?defat=".$_SESSION['defat'];
+  return $matches[1] . $rootURL . $plugin_url . "defavid.php?defat=".$_SESSION['defat'];
 }
 
 $mes = preg_replace_callback("/(<video[^>]*src *= *[\"']?)([^\"']*)/i", getURL, $out2);
