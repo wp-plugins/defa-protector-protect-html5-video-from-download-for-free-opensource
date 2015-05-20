1f61b6c6c9e981727ab4316e919521311072adf5
juthawong <juthawong@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-20 10:53:07 +0000

    Create tag
    git-svn-id: https://plugins.svn.wordpress.org/defa-protector-protect-html5-video-from-download-for-free-opensource/tags/6.3@1164073 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ defaprotector.php
@@ -0,0 +1,27 @@
+<?php
+/*
+Plugin Name: Defa Protector Platinum
+Plugin URI: http://ww2.juthawong.com/defaprotector
+Description: Protect your video from being downloaded or stolen
+Version: 6.3
+Author: Juthawong Naisanguansee
+Author URI: http://www.juthawong.com/
+License: GPL
+*/
+add_action('wp_head','defago');
+add_action('wp_footer', 'defaset');
+
+function defago()
+{
+include( plugin_dir_path( __FILE__ ) . 'includetop.php');
+
+}
+function defaset()
+{
+
+include( plugin_dir_path( __FILE__ ) . 'includebottom.php');
+}
+
+
+
+?>
---
+++ includebottom.php
@@ -0,0 +1,58 @@
+<?php
+//Written By Juthawong Naisanguansee
+if(session_id() == ''){
+     session_start(); 
+}
+error_reporting(0);
+$out2 = ob_get_contents();
+if(strpos($out2, "<video")||strpos($out2, "<audio")||strpos($out2, "<source")){
+     ob_clean();
+$file = dirname(__FILE__) . '/defaprotector.php';
+$plugin_url= plugin_dir_url($file);
+$plugin_url = wp_make_link_relative($plugin_url);
+
+$out2 = ob_get_clean();
+if(strpos($out2,"<safe")==false){
+$window = md5(time());
+$_SESSION['window'] = $window;
+?>
+<script>
+jQuery.ajax({
+  type: "POST",
+  url: "<?php echo $plugin_url;?>enable.php",
+});
+</script>
+  <?php
+if(strpos($out2,"<safe")!==false){
+$_SESSION['safe']="SAFE";
+}
+function getURL($matches) {
+  global $rootURL;
+if($_SESSION['defat']==""){
+$_SESSION['defat'] = 1;
+}else{
+$_SESSION['defat'] = $_SESSION['defat'] + 1;
+}
+$file = dirname(__FILE__) . '/defaprotector.php';
+$defa= plugin_dir_url($file);
+$defa = wp_make_link_relative($defa);
+$_SESSION['x'.$matches['2'].$_SESSION['defat']]=0;
+$_SESSION['defa'.$matches['2'].$_SESSION['defat']] = md5(time()."Defa Protector");
+$_SESSION['imdefa'.$_SESSION['defat']]=md5('Defa').base64_encode(base64_encode($matches['2']));
+$_SESSION['x'.$matches['2']]=0;
+$_SESSION['defa'.$matches['2']] = md5(time()."Defa Protector");
+$_SESSION['file'.$_SESSION['defat']] = md5('Defa').base64_encode(base64_encode($matches['2']));
+  return $matches[1] . $rootURL .$defa . "defavid.php?window=".$_SESSION['window']."&defat=".$_SESSION['defat'];
+}
+
+$mes = preg_replace_callback("/(<video[^>]*src *= *[\"']?)([^\"']*)/i", getURL, $out2);
+
+
+$mes = preg_replace_callback("/(<source[^>]*src *= *[\"']?)([^\"']*)/i", getURL, $mes);
+
+$mes = preg_replace_callback("/(<audio[^>]*src *= *[\"']?)([^\"']*)/i", getURL, $mes);
+echo $mes;
+}else{
+echo $out2;
+}}
+?>
